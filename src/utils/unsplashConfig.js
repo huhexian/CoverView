201e81d9b46da8274817b78282e5b25081e94707
青山 <42113492+huhexian@users.noreply.github.com>
2023-07-15 08:40:56 +0800

    Update unsplashConfig.js

--- src/utils/unsplashConfig.js
+++ src/utils/unsplashConfig.js
@@ -2,7 +2,7 @@ import { createApi } from 'unsplash-js';
 
 const key= process.env.REACT_APP_API_ACCESS_KEY
 const unsplash = createApi({
-    accessKey: key
+    accessKey: B0VMMOSb8AqDIG09iwE4ZqnSXX-JX0jt2cdWP9AmBQ4
 });
 
 export default unsplash;
